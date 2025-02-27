d59e9a69316563462aa0063c233b5ddbf87b0158
ChaoticAnisha <anishashah0117>
2025-02-27 15:46:48 +0545

    Adapter

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,7 +62,16 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.mockito:mockito-core:5.0.0")
     testImplementation("org.mockito.kotlin:mockito-kotlin:5.0.0")
-    testImplementation("androidx.arch.core:core-testing:2.2.0") // For ViewModel testing
+    testImplementation("androidx.arch.core:core-testing:2.2.0")
 
+        // Instrumented testing
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test:rules:1.5.0")
+    androidTestImplementation("androidx.test:runner:1.5.2")
+    androidTestImplementation("androidx.test.espresso:espresso-contrib:3.5.1")
+    androidTestImplementation("androidx.test.espresso:espresso-intents:3.5.1")
     }
+
+    
 
