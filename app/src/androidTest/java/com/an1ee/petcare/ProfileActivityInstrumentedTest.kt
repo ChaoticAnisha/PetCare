e45305f055eb4a16d2abfdf36ae947d9e347f1bf
ChaoticAnisha <anishashah0117>
2025-02-28 10:44:01 +0545

    Admin -User managements

--- app/src/androidTest/java/com/an1ee/petcare/ProfileActivityInstrumentedTest.kt
+++ app/src/androidTest/java/com/an1ee/petcare/ProfileActivityInstrumentedTest.kt
@@ -29,33 +29,26 @@ class ProfileActivityInstrumentedTest {
 
     @Before
     fun setUp() {
-        // Initialize Intents before each test
         Intents.init()
 
-        // Launch the activity
         activityScenario = ActivityScenario.launch(ProfileActivity::class.java)
     }
 
     @After
     fun tearDown() {
-        // Release Intents after each test
         Intents.release()
 
-        // Close the activity scenario
         activityScenario.close()
     }
 
     @Test
     fun testProfileActivityDisplaysCorrectly() {
-        // Check that the toolbar has the correct title
         onView(withId(R.id.toolbar))
             .check(matches(hasDescendant(withText("Profile"))))
 
-        // Check that the profile image is displayed
         onView(withId(R.id.image_profile))
             .check(matches(isDisplayed()))
 
-        // Check that the name text is displayed
         onView(withId(R.id.text_name))
             .check(matches(allOf(isDisplayed(), withText("John Doe"))))
 
@@ -71,7 +64,6 @@ class ProfileActivityInstrumentedTest {
         onView(withId(R.id.card_orders))
             .check(matches(isDisplayed()))
 
-        // Check that the orders recycler view is displayed
         onView(withId(R.id.recycler_orders))
             .check(matches(isDisplayed()))
     }
@@ -82,7 +74,6 @@ class ProfileActivityInstrumentedTest {
         onView(withId(R.id.button_home))
             .perform(click())
 
-        // Verify navigation to ProductDashboardActivity
         intended(hasComponent(ProductDashboardActivity::class.java.name))
     }
 
@@ -92,44 +83,34 @@ class ProfileActivityInstrumentedTest {
         onView(withId(R.id.button_cart))
             .perform(click())
 
-        // Verify navigation to ProductCartActivity
         intended(hasComponent(ProductCartActivity::class.java.name))
     }
 
     @Test
     fun testNavigationToOrder() {
-        // Click on "My Orders" text
         onView(withId(R.id.my_order))
             .perform(click())
 
-        // Verify navigation to OrderActivity
         intended(hasComponent(OrderActivity::class.java.name))
     }
 
     @Test
     fun testOrdersDisplayedInRecyclerView() {
-        // Verify the recycler view has at least 2 items (from the sample data)
         onView(withId(R.id.recycler_orders))
             .check(matches(hasMinimumChildCount(2)))
 
-        // Test can be extended to verify specific order content if test data is predictable
     }
 
     @Test
     fun testOrderItemInteraction() {
-        // This test assumes the first order in the recycler view can be clicked
-        // and that clicking it does something (you could expand this test based on your app behavior)
+
         onView(withId(R.id.recycler_orders))
             .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
 
-        // Add verification of what should happen when clicking an order item
-        // For example, if clicking shows details, you could verify a details view appears
-    }
 
-    // Helper method to create an intent with test data if needed
+    }
     private fun createProfileActivityIntent(): Intent {
         return Intent(ApplicationProvider.getApplicationContext(), ProfileActivity::class.java).apply {
-            // Add any extras needed for testing
         }
     }
 }
